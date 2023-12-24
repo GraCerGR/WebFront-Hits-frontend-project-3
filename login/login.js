33165b5b97a5b09cca3d3a7cc5c8943394d9088e
Grigoriy Sribnyy <ctrhtnysq.afqk@gmail.com>
2023-12-24 21:15:08 +0700

    js файл для логина Добавил логику для входа (из прошлого проекта)

---
+++ login/login.js
@@ -0,0 +1,45 @@
+let token;
+
+async function post(url, data = null) {
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      if (data.token) {
+        // Если успех, получаем токен из ответа
+        token = data.token;
+        // Далее можно выполнять другие действия с полученным токеном
+        // Например, сохранить его в localStorage и перенаправить пользователя на другую страницу
+      } else {
+        // Если ошибка, выводим сообщение об ошибке
+        alert(data.message || 'Неверный логин или пароль');
+      }
+      localStorage.setItem('token', token);
+    })
+    .catch(error => {
+      console.error('Ошибка', error);
+    });
+}
+  const url = "https://mis-api.kreosoft.space/api/doctor/login";
+const loginButton = document.getElementById('loginButton');
+if (loginButton){
+loginButton.addEventListener('click', function() {
+  const email = document.getElementById('Email').value;
+  const password = document.getElementById('password').value;
+  
+  const data = {
+    email: email,
+    password: password
+  };
+console.log(data);
+post(url, data);
+console.log(token);
+});}
+
+//export { token };
